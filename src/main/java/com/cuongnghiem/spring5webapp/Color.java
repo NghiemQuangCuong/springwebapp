503fa5ce644775cb69e0feda235badf465e37a44
CuongNghiem <quangcuongnghiem@gmail.com>
2021-08-31 17:22:00 +0700

    Init

---
+++ src/main/java/com/cuongnghiem/spring5webapp/Color.java
@@ -0,0 +1,10 @@
+package com.cuongnghiem.spring5webapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Color {
+    public void doSomething(){
+        System.out.println("this is color");
+    }
+}
---
+++ src/main/java/com/cuongnghiem/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,18 @@
+package com.cuongnghiem.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+            SpringApplication.run(Spring5webappApplication.class, args);
+
+        Color color = context.getBean(Color.class);
+        color.doSomething();
+    }
+
+}
