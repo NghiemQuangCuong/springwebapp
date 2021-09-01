154a1352fa6732019ff4220335abc800a9acacfa
CuongNghiem <quangcuongnghiem@gmail.com>
2021-09-01 23:23:49 +0700

    Spring MVC, BookController and AuthorController.

--- src/main/java/com/cuongnghiem/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/com/cuongnghiem/spring5webapp/bootstrap/BootStrapData.java
@@ -26,25 +26,37 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "123123");
-        eric.getBooks().add(ddd);
-        ddd.getAuthors().add(eric);
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
-
-        Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "12213125");
-        rod.getBooks().add(noEJB);
-        noEJB.getAuthors().add(rod);
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
-
         Publisher prh = new Publisher("Penguin Random House",
                 "1745 Broadway",
                 "New York",
                 "NY United States",
                 "10019");
+
+        publisherRepository.save(prh);
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        ddd.setPublisher(prh);
+        prh.getBooks().add(ddd);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "12213125");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        noEJB.setPublisher(prh);
+        prh.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
         publisherRepository.save(prh);
 
         System.out.println("Started in Bootstrap");
